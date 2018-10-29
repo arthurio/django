7eb6137631a705ffbef7bde13db8fb52659f88ed
Arthur Rio <arthur@minervaproject.com>
2018-10-29 12:25:41 -0700

    Update RenameContentType to use the post_operation signal instead of pre_migrate

--- django/contrib/contenttypes/apps.py
+++ django/contrib/contenttypes/apps.py
@@ -3,7 +3,8 @@ from django.contrib.contenttypes.checks import (
     check_generic_foreign_keys, check_model_name_lengths,
 )
 from django.core import checks
-from django.db.models.signals import post_migrate, pre_migrate
+from django.db import migrations
+from django.db.models.signals import post_migrate, post_operation
 from django.utils.translation import gettext_lazy as _
 
 from .management import (
@@ -16,7 +17,7 @@ class ContentTypesConfig(AppConfig):
     verbose_name = _("Content Types")
 
     def ready(self):
-        pre_migrate.connect(inject_rename_contenttypes_operations, sender=self)
+        post_operation.connect(inject_rename_contenttypes_operations, sender=migrations.RenameModel)
         post_migrate.connect(create_contenttypes)
         checks.register(check_generic_foreign_keys, checks.Tags.models)
         checks.register(check_model_name_lengths, checks.Tags.models)
--- django/contrib/contenttypes/management/__init__.py
+++ django/contrib/contenttypes/management/__init__.py
@@ -42,46 +42,12 @@ class RenameContentType(migrations.RunPython):
         self._rename(apps, schema_editor, self.new_model, self.old_model)
 
 
-def inject_rename_contenttypes_operations(plan=None, apps=global_apps, using=DEFAULT_DB_ALIAS, **kwargs):
-    """
-    Insert a `RenameContentType` operation after every planned `RenameModel`
-    operation.
-    """
-    if plan is None:
-        return
-
-    # Determine whether or not the ContentType model is available.
-    try:
-        ContentType = apps.get_model('contenttypes', 'ContentType')
-    except LookupError:
-        available = False
-    else:
-        if not router.allow_migrate_model(using, ContentType):
-            return
-        available = True
-
-    for migration, backward in plan:
-        if (migration.app_label, migration.name) == ('contenttypes', '0001_initial'):
-            # There's no point in going forward if the initial contenttypes
-            # migration is unapplied as the ContentType model will be
-            # unavailable from this point.
-            if backward:
-                break
-            else:
-                available = True
-                continue
-        # The ContentType model is not available yet.
-        if not available:
-            continue
-        inserts = []
-        for index, operation in enumerate(migration.operations):
-            if isinstance(operation, migrations.RenameModel):
-                operation = RenameContentType(
-                    migration.app_label, operation.old_name_lower, operation.new_name_lower
-                )
-                inserts.append((index + 1, operation))
-        for inserted, (index, operation) in enumerate(inserts):
-            migration.operations.insert(inserted + index, operation)
+def inject_rename_contenttypes_operations(migration, operation, **kwargs):
+    return [
+        RenameContentType(
+            migration.app_label, operation.old_name_lower, operation.new_name_lower
+        )
+    ]
 
 
 def get_contenttypes_and_models(app_config, using, ContentType):
